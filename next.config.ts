a1e64707d2110e43bee3c6fba1d299142b33961a
Maurice Mack <14870656+moujou@users.noreply.github.com>
2026-08-06 19:28:08 +0200

    chore: rename to aquavitaeum + add basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/aquavitaeum",
+  assetPrefix: "/aquavitaeum",
   images: {
     unoptimized: true,
   },
